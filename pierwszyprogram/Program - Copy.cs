55097730e80f6d96fc8b76ee2188bfd3f6bd6e33
Opjum <krasher736@gmail.com>
2020-05-28 23:21:12 +0200

    Add project files.

---
+++ pierwszyprogram/Program - Copy.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace pierwszyprogram
+{
+    class Program123
+    {
+        static void Main()
+        {
+
+            do
+            {
+                Console.ResetColor();
+                welcomemessage();
+                int player1 = input1();
+                int player2 = input2();
+
+                string winner = whowin2(player1, player2);
+                winMessage(winner);
+            } while (playagain());
+            
+
+            Console.ReadLine();
+        }
+
+        static void welcomemessage()
+        {
+            Console.WriteLine("Welcome in rock, paper, scissors game");
+            Console.WriteLine("There is three types to choose : \n" +
+                "1. rock \n" +
+                "2. paper \n " +
+                "3. scissors \n" +
+                    "please choose NUMBER 1-3 ");
+
+
+        }
+
+        static int input1()
+        {
+            Console.WriteLine("Player 1 please input your number");
+            return Convert.ToInt32(Console.ReadLine());
+
+        }
+        static int input2()
+        {
+            Console.WriteLine("Player 2 please input your number");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+
+        static void winMessage(string winner)
+        {
+            if (winner == "player1")
+            {
+                colorchange(ConsoleColor.Yellow);
+                Console.WriteLine("Player 1 WIN");
+            }
+            else if (winner == "player2")
+            {
+                colorchange(ConsoleColor.Green);
+                Console.WriteLine("Player 2 WIN");
+            }
+            else
+            {
+                colorchange(ConsoleColor.Red);
+                Console.WriteLine("its a draw");
+            }
+
+
+        }
+
+        static string whowin2(int x, int y)
+        {
+            // x- player 1 input, y- player 2 input
+            // 1 - rock, 2- paper, 3 = scissors
+            Dictionary<int, int> player1win = new Dictionary<int, int>() {
+                { 1, 3 },
+                { 2, 1 },
+                { 3, 2 }
+                };
+             Dictionary<int, int> player2win = new Dictionary<int, int>() {
+                { 1, 2 },
+                { 2, 3 },
+                { 3, 1 }
+                   };
+
+            string win1 = "";
+
+            if (player1win.ContainsKey(x) && player1win[x].Equals(y))
+            {
+                win1 = "player1";
+            }
+            else if (player2win.ContainsKey(x) && player2win[x].Equals(y))
+            {
+                win1 = "player2";
+            }
+            else if (x == y)
+            {
+                win1 = "draw";
+            }
+            else
+            {
+                Console.WriteLine("something wrong");
+            }
+            return win1;
+
+
+        }
+        static void colorchange(ConsoleColor x)
+        {
+            Console.ForegroundColor = x;
+            Console.Clear();
+        }
+
+
+        static bool playagain()
+        {
+            Console.WriteLine("czy chcesz grac dalej Y/N ");
+            char answer = Convert.ToChar(Console.ReadLine().ToUpper());
+            if (answer == 'Y')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+	
+    }
+}
---
+++ pierwszyprogram/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace pierwszyprogram
+{
+    class Program
+    {
+        static void  aain()
+        {
+
+            string[,] multiarray = { { "jan", "rodo"},
+                { "krzysztof", "kononowicz"}, {"Major", "Suchodolski" } }; 
+            for (int i = 0; i < multiarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiarray.GetLength(1); j++)
+                {
+                    Console.Write(" {0}", multiarray[i, j].ToUpper());
+                }
+                Console.WriteLine();
+            }
+
+            // drugi blok tk
+            
+            int[] tablica = {20, 40, 60, 100};
+            int[] tablica1 = new int[10];
+            Array tablica2 = Array.CreateInstance(typeof(int), 10);
+
+            StringBuilder sb = new StringBuilder("RANDOM TEXT");
+            
+            
+
+
+            Console.ReadKey();
+        }
+
+
+        static void PrintArray(Array intArray, string mess)
+        {
+            foreach (int i in intArray)
+            {
+               
+                    Console.WriteLine("{0} : {1} ", i, mess);
+                
+            }
+        }
+
+
+        private static void SayHello()
+        {
+            
+            Console.Write("What is your name  ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+
+            Console.ReadKey();
+        }
+    }
+}
+/*
+namespace Drugiprogram
+{
+    class Program2
+    {
+
+
+        static void UMBO(string[] args)
+        {
+
+
+        }
+    }
+}
+*/
+
+
